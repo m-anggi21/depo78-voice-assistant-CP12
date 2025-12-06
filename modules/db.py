81ad2d15cb6a622fd98cd90b04bb498efaa1ef9e
Muhamad Anggi Saputra <muhamadanggi2104@gmail.com>
2025-12-06 20:42:27 +0700

    Add get_db function for PostgreSQL connection
    Implement database connection function using psycopg2.

---
+++ modules/db.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import psycopg2
+import psycopg2.extras
+
+def get_db():
+    conn = psycopg2.connect(
+        host=st.secrets["DB_HOST"],
+        port=st.secrets["DB_PORT"],
+        dbname=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASS"]
+    )
+    return conn
