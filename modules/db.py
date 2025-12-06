4d227dce5cedb1d8f1f9fa70f321b2af2f0463b6
Muhamad Anggi Saputra <muhamadanggi2104@gmail.com>
2025-12-06 21:47:24 +0700

    Update db.py

--- modules/db.py
+++ modules/db.py
@@ -1,12 +1,13 @@
-import os
+import streamlit as st
 import psycopg2
 import psycopg2.extras
 
 def get_db():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+    conn = psycopg2.connect(
+        host=st.secrets["DB_HOST"],
+        port=st.secrets["DB_PORT"],
+        dbname=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASS"]
     )
+    return conn
