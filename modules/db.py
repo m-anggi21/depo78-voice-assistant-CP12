73ed50fdbeaa4933b4d3a0d70cc91cd017675dfd
Muhamad Anggi Saputra <muhamadanggi2104@gmail.com>
2025-12-06 21:39:03 +0700

    Refactor DB connection to use environment variables

--- modules/db.py
+++ modules/db.py
@@ -1,13 +1,12 @@
-import streamlit as st
+import os
 import psycopg2
 import psycopg2.extras
 
 def get_db():
-    conn = psycopg2.connect(
-        host=st.secrets["DB_HOST"],
-        port=st.secrets["DB_PORT"],
-        dbname=st.secrets["DB_NAME"],
-        user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASS"]
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
-    return conn
