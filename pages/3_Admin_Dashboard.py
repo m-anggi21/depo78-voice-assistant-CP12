47784bcc63cdea6a0fdefa8dc6249873a558f441
Muhamad Anggi Saputra <muhamadanggi2104@gmail.com>
2026-01-07 22:45:43 +0700

    Delete pages/3_Admin_Dashboard.py

--- pages/3_Admin_Dashboard.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from modules.admin_actions import admin_page
-
-if st.session_state.get("role") != "admin":
-    st.error("Halaman ini hanya untuk admin.")
-else:
-    admin_page()
